37108f089541a18c25079a1ac92b5b558f065bb4
ShiwankRajput <shiwannnnk@gmail.com>
2025-07-27 23:08:29 +0530

    introducing to NexMed

---
+++ NexMed/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Features from './Pages/Features/Features';
+import HowItWorks from './Pages/HowItWorks/Works';
+import About from './Pages/About/About';
+import Contact from './Pages/Contact/Contact';
+import LoginSignUp from './Pages/LoginSignUp/LoginSignUp';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/home" element={<Home/>} />
+        <Route path="/features" element={<Features/>} />
+        <Route path="/howItWorks" element={<HowItWorks/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/loginSignUp" element={<LoginSignUp/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
